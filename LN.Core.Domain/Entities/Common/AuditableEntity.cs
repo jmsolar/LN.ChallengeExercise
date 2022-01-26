b2b9000bdcc0a7791d65cbf07ca1bc0e526afe74
Jonatan Matias Solar <matias.solar@outlook.com>
2022-01-26 11:27:00 -0300

    Agrego nueva entidad AuditableEntity

---
+++ LN.Core.Domain/Entities/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LN.Core.Domain.Entities.Common
+{
+    public abstract class AuditableEntity : BaseEntity
+    {
+        public Guid CreatedBy { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
